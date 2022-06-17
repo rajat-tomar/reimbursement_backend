3af1ef707d0b125a2ecfde50548fad2f150c6636
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-06-17 11:45:56 +0530

    adds test for expense creation failure in expense service

--- service/expense_test.go
+++ service/expense_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"reimbursement_backend/model"
 	"testing"
@@ -19,8 +20,27 @@ func (m mockExpenseRepository) GetAll() ([]model.Expense, error) {
 	panic("implement me")
 }
 
-func (m mockExpenseRepository) Create(expense model.Expense) (int, error) {
+func (m mockExpenseRepository) Create(e model.Expense) (int, error) {
+	if e.Amount == -1 {
+		return 0, errors.New("error creating expense")
+	}
+
 	return 7, nil
+}
+
+func Test_Expense_Service_Create_Returns_Error_When_Unable_To_Create(t *testing.T) {
+	expense := model.Expense{
+		ID:     0,
+		Amount: -1,
+	}
+
+	expenseService := expenseService{
+		expenseRepository: &mockExpenseRepository{},
+	}
+
+	_, err := expenseService.Create(expense)
+
+	assert.Equal(t, errors.New("error creating expense"), err)
 }
 
 func TestCreateExpense(t *testing.T) {
