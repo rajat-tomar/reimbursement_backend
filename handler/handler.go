f806b6fa211b6ccf694f12f017bb82e70e65d15d
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-06-22 17:34:52 +0530

    return expense by fetching id from query param

--- handler/handler.go
+++ handler/handler.go
@@ -4,9 +4,18 @@ import (
 	"encoding/json"
 	"net/http"
 	"reimbursement_backend/model"
+	"strconv"
 )
 
 func GetExpenseById(w http.ResponseWriter, r *http.Request) {
-	expense := model.Expense{Id: 20, Amount: 1000}
+	var expense model.Expense
+	q := r.URL.Query()
+	id, _ := strconv.Atoi(q.Get("id"))
+	if id == 20 {
+		expense = model.Expense{Id: 20, Amount: 1000}
+	}
+	if id == 10 {
+		expense = model.Expense{Id: 10, Amount: 500}
+	}
 	json.NewEncoder(w).Encode(expense)
 }
--- handler/handler_test.go
+++ handler/handler_test.go
@@ -10,18 +10,33 @@ import (
 )
 
 func TestGetExpenseById(t *testing.T) {
-	request, _ := http.NewRequest(http.MethodGet, "/expense", nil)
-	response := httptest.NewRecorder()
+	t.Run("returns expense with id 20", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/expense?id=20", nil)
+		response := httptest.NewRecorder()
+		GetExpenseById(response, request)
+		var expense model.Expense
+		err := json.NewDecoder(response.Body).Decode(&expense)
+		got := expense
+		want := model.Expense{
+			Id:     20,
+			Amount: 1000,
+		}
+		assert.Equal(t, nil, err)
+		assert.Equal(t, want, got)
+	})
 
-	GetExpenseById(response, request)
-
-	var expense model.Expense
-	err := json.NewDecoder(response.Body).Decode(&expense)
-	got := expense
-	want := model.Expense{
-		Id:     20,
-		Amount: 1000,
-	}
-	assert.Equal(t, nil, err)
-	assert.Equal(t, want, got)
+	t.Run("returns expense with id 10", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/expense?id=10", nil)
+		response := httptest.NewRecorder()
+		GetExpenseById(response, request)
+		var expense model.Expense
+		err := json.NewDecoder(response.Body).Decode(&expense)
+		got := expense
+		want := model.Expense{
+			Id:     10,
+			Amount: 500,
+		}
+		assert.Equal(t, nil, err)
+		assert.Equal(t, want, got)
+	})
 }
