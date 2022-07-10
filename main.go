ce5211eabc7bbe75e2305de3ebf6148ec3e2fa9c
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-11 01:49:30 +0530

    Update cors an route to google login

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
+	"log"
 	"net/http"
 	"reimbursement_backend/api"
 	"reimbursement_backend/config"
-	"reimbursement_backend/util"
+	"reimbursement_backend/utils"
 )
 
 func runServer() {
@@ -18,21 +19,32 @@ func runServer() {
 	}
 	address := fmt.Sprintf(":%d", port)
 	router := mux.NewRouter()
-	handler := cors.AllowAll().Handler(router)
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"https://accounts.google.com/", "http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-Requested-With"},
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+		AllowCredentials: true,
+	})
+	handler := c.Handler(router)
 	controllers := api.NewControllers()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("{\"hello\": \"world\"}"))
+		if _, err := w.Write([]byte("{\"hello\": \"world\"}")); err != nil {
+			log.Println(err)
+		}
+		w.WriteHeader(http.StatusOK)
 	})
-
-	router.HandleFunc("/google/login", controllers.OAuthController.GoogleLogin).Methods("GET")
+	router.HandleFunc("/login", controllers.OAuthController.GoogleLogin).Methods("GET")
 	router.HandleFunc("/auth/google/callback", controllers.OAuthController.GoogleCallback).Methods("GET")
 	router.HandleFunc("/expense", controllers.ExpenseController.CreateExpense).Methods("POST")
 	router.HandleFunc("/expenses", controllers.ExpenseController.GetExpenses).Methods("GET")
 	router.HandleFunc("/expense", controllers.ExpenseController.DeleteExpense).Methods("DELETE")
 
-	http.ListenAndServe(address, handler)
+	log.Println("Server is running on port", port)
+	log.Fatal(http.ListenAndServe(address, handler))
 }
 
 func main() {
@@ -40,6 +52,6 @@ func main() {
 	config.InitLogger()
 	config.InitDb()
 	defer config.CloseDb()
-	util.ExecuteCommands()
+	utils.ExecuteCommands()
 	runServer()
 }
