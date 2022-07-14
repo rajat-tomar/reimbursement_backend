bd541aa5bcf44dad3f395ac51e201c9c17857bd9
Rajat Tomar <rajat.rajattomar@gmail.com>
2022-07-14 08:12:09 +0530

    Refactor user service

--- service/user.go
+++ service/user.go
@@ -1,12 +1,12 @@
 package service
 
 import (
-	"fmt"
 	"reimbursement_backend/model"
 	"reimbursement_backend/repository"
 )
 
 type UserService interface {
+	FindByEmail(email string) (model.User, error)
 	CreateUser(user model.User) (model.User, error)
 }
 
@@ -20,13 +20,20 @@ func NewUserService() *userService {
 	}
 }
 
-func (us *userService) CreateUser(user model.User) (model.User, error) {
-	user, err := us.userRepository.FindByEmail(user.Email)
-	if err == nil {
-		return model.User{}, fmt.Errorf("user already exists %v", err)
+func (us *userService) FindByEmail(email string) (model.User, error) {
+	user, err := us.userRepository.FindByEmail(email)
+	if err != nil {
+		return model.User{}, err
 	}
 
-	user, err = us.userRepository.CreateUser(user)
+	return user, nil
+}
 
-	return user, err
+func (us *userService) CreateUser(user model.User) (model.User, error) {
+	user, err := us.userRepository.CreateUser(user)
+	if err != nil {
+		return model.User{}, err
+	}
+
+	return user, nil
 }
